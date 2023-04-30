b6115c4b77e1cba6d6284bb3deb538fef09afcc1
Bogdan <112793886+pavyk-shnyk@users.noreply.github.com>
2023-04-30 10:42:22 +0600

    fcxv
    xcvx

--- app/src/main/java/com/nikita/mlkit_camera_app/cameraFeature/CameraScanActivity.kt
+++ app/src/main/java/com/nikita/mlkit_camera_app/cameraFeature/CameraScanActivity.kt
@@ -3,11 +3,23 @@ package com.nikita.mlkit_camera_app.cameraFeature
 import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.google.common.util.concurrent.ListenableFuture
 import com.nikita.mlkit_camera_app.R
 
-class CameraScanActivity: Activity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+class CameraScanActivity: AppCompatActivity() {
+    private lateinit var cameraFuture: ListenableFuture<ProcessCameraProvider>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan_camera)
+
+        cameraFuture = ProcessCameraProvider.getInstance(this)
+        cameraFuture.addListener({
+
+
+        }, ContextCompat.getMainExecutor(this))
     }
 }
--- app/src/main/java/com/nikita/mlkit_camera_app/start_Scan/StartScan.kt
+++ app/src/main/java/com/nikita/mlkit_camera_app/start_Scan/StartScan.kt
@@ -1,14 +1,33 @@
 package com.nikita.mlkit_camera_app.start_Scan
 
+
+import android.Manifest
+import android.app.Instrumentation.ActivityResult
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.nikita.mlkit_camera_app.R
+import com.nikita.mlkit_camera_app.cameraFeature.CameraScanActivity
 
 class StartScan: Fragment() {
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+        {isGranted:Boolean ->
+            if(isGranted == true){
+                startScan()
+            }else{
+            }
+        }
+    )
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,8 +40,20 @@ class StartScan: Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val startScanButton = view.findViewById<Button>(R.id.start_camera_button)
         startScanButton.setOnClickListener {
+            val permission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+            when (permission){
+                PackageManager.PERMISSION_GRANTED -> {
+                    startScan()
+                }else -> {
+                    permissionLauncher.launch(Manifest.permission.CAMERA)
+                }
+            }
             
         }
+    }
+    private fun startScan(){
+        val intent = Intent(requireContext(), CameraScanActivity::class.java)
+        startActivity(intent)
     }
 
 
